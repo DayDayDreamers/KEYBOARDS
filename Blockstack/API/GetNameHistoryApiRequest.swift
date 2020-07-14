81955a990f2859a65d77e2d09fbecea0ec884649
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2020-07-14 21:17:15 +0000

    Fixes #132

--- Blockstack/API/GetNameHistoryApiRequest.swift
+++ Blockstack/API/GetNameHistoryApiRequest.swift
@@ -8,4 +8,15 @@ class GetNameHistoryApiRequest: NSObject {
         let url = "https://core.blockstack.org/v1/names/\(name)/history"
         
         Alamofire.request(url)
-            .val
+            .validate()
+            .responseDecodableObject(decoder: JSONDecoder())
+            {(response: DataResponse<Dictionary<String, [OperationModel]>>)
+                in
+                if self.delegate == nil {
+                    return
+                }
+                
+                switch response.result {
+                case .success(let value):
+                    self.delegate!.resultReceived(data:value)
+             
