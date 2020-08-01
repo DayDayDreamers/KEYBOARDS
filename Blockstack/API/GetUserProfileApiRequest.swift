61e967f8a140f9c7b97fa4c6495885eac130feee
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2020-08-01 09:09:29 +0000

    Rename some methods

--- Blockstack/API/GetUserProfileApiRequest.swift
+++ Blockstack/API/GetUserProfileApiRequest.swift
@@ -1,1 +1,14 @@
-imp
+import Foundation
+import Alamofire
+
+class GetUserProfileApiRequest: NSObject {
+    let path = "/v1/users/"
+    var delegate : ApiCallback? = nil
+   
+    func DispatchWithName(name : String) {
+        let url = "https://core.blockstack.org\(path)\(name)"
+        
+        Alamofire.request(url)
+            .validate()
+            .responseDecodableObject(keyPath:name, decoder: JSONDecoder())
+         
