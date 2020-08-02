7f72bc2fe12356df08a65e2f7de66689c0ddbd07
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2020-08-02 19:19:55 +0000

    mac_rhosim_autocommit

--- Blockstack/API/GetUserProfileApiRequest.swift
+++ Blockstack/API/GetUserProfileApiRequest.swift
@@ -11,4 +11,13 @@ class GetUserProfileApiRequest: NSObject {
         Alamofire.request(url)
             .validate()
             .responseDecodableObject(keyPath:name, decoder: JSONDecoder())
-         
+                {(response: DataResponse<UserProfileModel>) in
+                    if self.delegate == nil {
+                        return
+                    }
+                    
+                    switch response.result {
+                    case .success(let value):
+                        self.delegate!.resultReceived(data:value)
+                    case .failure(let error):
+                        s
