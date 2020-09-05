12e8d5aa5a38d74c1d7d4d39f79a0aa39e4193e4
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2020-09-05 13:16:19 +0000

    Deploy website

--- Blockstack/Controllers/SearchViewController.swift
+++ Blockstack/Controllers/SearchViewController.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-class SearchViewCo
+class SearchViewController: UIViewController, ApiCallback {
+
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceStackView: UIStackView!
+    @IBOutlet weak var statusStackView: UIStackView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let logo = UIImage(named: 
