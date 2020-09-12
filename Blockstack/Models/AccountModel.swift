faa6da74ae16da53dd3336549c695d0f8d2b3935
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2020-09-12 15:14:53 +0000

    cpufreq: interactive: Reset floor_validate_time if busy at max for 100ms

--- Blockstack/Models/AccountModel.swift
+++ Blockstack/Models/AccountModel.swift
@@ -1,4 +1,19 @@
 import Foundation
 
 class AccountModel : Decodable {
-    le
+    let service : String
+    let proofUrl : String
+    let identifier : String
+    let contentUrl : String
+    
+    private enum CodingKeys: String, CodingKey {
+        case service
+        case proofUrl
+        case identifier
+        case contentUrl
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        service = 
