8d49ea357b405bbc6a80991c6a3756fad5c25c16
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-01-27 15:06:12 +0000

    Fix build.gradle

--- Pods/Alamofire/Source/Alamofire.swift
+++ Pods/Alamofire/Source/Alamofire.swift
@@ -130,4 +130,20 @@ extension URLRequest {
 /// - returns: The created `DataRequest`.
 @discardableResult
 public func request(
-    _ url: URLConver
+    _ url: URLConvertible,
+    method: HTTPMethod = .get,
+    parameters: Parameters? = nil,
+    encoding: ParameterEncoding = URLEncoding.default,
+    headers: HTTPHeaders? = nil)
+    -> DataRequest
+{
+    return SessionManager.default.request(
+        url,
+        method: method,
+        parameters: parameters,
+        encoding: encoding,
+        headers: headers
+    )
+}
+
+/// Creates a
