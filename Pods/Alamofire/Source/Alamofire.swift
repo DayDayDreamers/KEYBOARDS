9957ef21d1fd6fd53bfdc48b423948352ba63e5e
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-02-17 08:59:24 +0000

    Ignore coverage

--- Pods/Alamofire/Source/Alamofire.swift
+++ Pods/Alamofire/Source/Alamofire.swift
@@ -351,4 +351,9 @@ public func upload(_ stream: InputStream, with urlRequest: URLRequestConvertible
 // MARK: MultipartFormData
 
 /// Encodes `multipartFormData` using `encodingMemoryThreshold` with the default `SessionManager` and calls
-/// `encodingCompletion` with new `UploadReq
+/// `encodingCompletion` with new `UploadRequest` using the `url`, `method` and `headers`.
+///
+/// It is important to understand the memory implications of uploading `MultipartFormData`. If the cummulative
+/// payload is small, encoding the data in-memory and directly uploading to a server is the by far the most
+/// efficient approach. However, if the payload is too large, encoding the data in-memory could cause your app to
+/// be terminated. Larger pa
