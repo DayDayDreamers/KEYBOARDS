69b9cfaa80af8c7625a867dc69c8916da7c36f75
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-02-17 12:31:52 +0000

    add jquery

--- Pods/Alamofire/Source/Alamofire.swift
+++ Pods/Alamofire/Source/Alamofire.swift
@@ -356,4 +356,9 @@ public func upload(_ stream: InputStream, with urlRequest: URLRequestConvertible
 /// It is important to understand the memory implications of uploading `MultipartFormData`. If the cummulative
 /// payload is small, encoding the data in-memory and directly uploading to a server is the by far the most
 /// efficient approach. However, if the payload is too large, encoding the data in-memory could cause your app to
-/// be terminated. Larger pa
+/// be terminated. Larger payloads must first be written to disk using input and output streams to keep the memory
+/// footprint low, then the data can be uploaded as a stream from the resulting file. Streaming from disk MUST be
+/// used for larger payloads such as video content.
+///
+/// The `encodingMemoryThreshold` parameter allows Alamofire to automatically determine whether to encode in-memory
+/// or stream from disk. If the content lengt
