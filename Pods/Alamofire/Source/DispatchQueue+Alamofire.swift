37dc95ba30cc48cbf108ed5ab6954aa8afe053b2
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-03-04 18:21:44 +0000

    removed commented-out code

--- Pods/Alamofire/Source/DispatchQueue+Alamofire.swift
+++ Pods/Alamofire/Source/DispatchQueue+Alamofire.swift
@@ -28,4 +28,10 @@ import Foundation
 extension DispatchQueue {
     static var userInteractive: DispatchQueue { return DispatchQueue.global(qos: .userInteractive) }
     static var userInitiated: DispatchQueue { return DispatchQueue.global(qos: .userInitiated) }
-    static
+    static var utility: DispatchQueue { return DispatchQueue.global(qos: .utility) }
+    static var background: DispatchQueue { return DispatchQueue.global(qos: .background) }
+
+    func after(_ delay: TimeInterval, execute closure: @escaping () -> Void) {
+        asyncAfter(deadline: .now() + delay, execute: closure)
+    }
+}
---
+++ Pods/Alamofire/Source/MultipartFormData.swift
@@ -0,0 +1,4 @@
+//
+//  MultipartFormData.swift
+//
+//  Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
