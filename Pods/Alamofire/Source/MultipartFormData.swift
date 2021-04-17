ba8e081dbfef826691841c1536984e79bed6fe7a
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-04-17 13:29:42 +0000

    Review comments

--- Pods/Alamofire/Source/MultipartFormData.swift
+++ Pods/Alamofire/Source/MultipartFormData.swift
@@ -458,4 +458,11 @@ open class MultipartFormData {
 
     private func write(_ bodyPart: BodyPart, to outputStream: OutputStream) throws {
         try writeInitialBoundaryData(for: bodyPart, to: outputStream)
-        try writeHeaderData(for: 
+        try writeHeaderData(for: bodyPart, to: outputStream)
+        try writeBodyStream(for: bodyPart, to: outputStream)
+        try writeFinalBoundaryData(for: bodyPart, to: outputStream)
+    }
+
+    private func writeInitialBoundaryData(for bodyPart: BodyPart, to outputStream: OutputStream) throws {
+        let initialData = bodyPart.hasInitialBoundary ? initialBoundaryData() : encapsulatedBoundaryData()
+        return try write(initialData, to:
