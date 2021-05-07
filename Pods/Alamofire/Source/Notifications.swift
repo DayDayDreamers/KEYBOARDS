c71b8fd96c8f209f01bf7baabff7d245c989f064
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-05-07 18:34:01 +0000

    Fix lint issue.

--- Pods/Alamofire/Source/Notifications.swift
+++ Pods/Alamofire/Source/Notifications.swift
@@ -31,4 +31,10 @@ extension Notification.Name {
         public static let DidResume = Notification.Name(rawValue: "org.alamofire.notification.name.task.didResume")
 
         /// Posted when a `URLSessionTask` is suspended. The notification `object` contains the suspended `URLSessionTask`.
-        public static let DidSuspend = Notification.Name(rawValue: "org.alamofire.notification.nam
+        public static let DidSuspend = Notification.Name(rawValue: "org.alamofire.notification.name.task.didSuspend")
+
+        /// Posted when a `URLSessionTask` is cancelled. The notification `object` contains the cancelled `URLSessionTask`.
+        public static let DidCancel = Notification.Name(rawValue: "org.alamofire.notification.name.task.didCancel")
+
+        /// Posted when a `URLSessionTask` is completed. The notification `object` contains the completed `URLSessionTask`.
+        public static le
