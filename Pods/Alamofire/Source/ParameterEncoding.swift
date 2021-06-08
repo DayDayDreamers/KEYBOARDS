2550fbe71db22e567f3f8928b8c0ff327b182a4d
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-06-08 07:25:02 +0000

    1.0.0.0

--- Pods/Alamofire/Source/ParameterEncoding.swift
+++ Pods/Alamofire/Source/ParameterEncoding.swift
@@ -233,4 +233,10 @@ public struct URLEncoding: ParameterEncoding {
     ///
     /// RFC 3986 states that the following characters are "reserved" characters.
     ///
-    /// - Gene
+    /// - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+    /// - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+    ///
+    /// In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+    /// query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+    /// should be percent-escaped in the query string.
+  
