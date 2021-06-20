d6eaae51f95faed44adc907537ace019fd2b790e
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-06-20 16:57:01 +0000

    resolve merge conflict

--- Pods/Alamofire/Source/ParameterEncoding.swift
+++ Pods/Alamofire/Source/ParameterEncoding.swift
@@ -353,4 +353,13 @@ public struct JSONEncoding: ParameterEncoding {
     ///
     /// - returns: The encoded request.
     public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
-        var urlRequest = 
+        var urlRequest = try urlRequest.asURLRequest()
+
+        guard let parameters = parameters else { return urlRequest }
+
+        do {
+            let data = try JSONSerialization.data(withJSONObject: parameters, options: options)
+
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+          
