8b51a2633987fca3c9cbf7c99e8666d3a8b28630
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2021-07-11 12:42:43 +0000

    update security

--- Pods/Alamofire/Source/ParameterEncoding.swift
+++ Pods/Alamofire/Source/ParameterEncoding.swift
@@ -454,4 +454,14 @@ public struct PropertyListEncoding: ParameterEncoding {
     public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
         var urlRequest = try urlRequest.asURLRequest()
 
-        guard let parameters = par
+        guard let parameters = parameters else { return urlRequest }
+
+        do {
+            let data = try PropertyListSerialization.data(
+                fromPropertyList: parameters,
+                format: format,
+                options: options
+            )
+
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/x-plist", forHTTPHeaderField: "
