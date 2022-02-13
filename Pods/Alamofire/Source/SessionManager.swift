435ec4e787907100b2acc8e05a50226718b53d28
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2022-02-13 05:51:38 +0000

    Remove unused lines

--- Pods/Alamofire/Source/SessionManager.swift
+++ Pods/Alamofire/Source/SessionManager.swift
@@ -32,4 +32,10 @@ open class SessionManager {
     /// Defines whether the `MultipartFormData` encoding was successful and contains result of the encoding as
     /// associated values.
     ///
-    /// - Success: Represents a succe
+    /// - Success: Represents a successful `MultipartFormData` encoding and contains the new `UploadRequest` along with
+    ///            streaming information.
+    /// - Failure: Used to represent a failure in the `MultipartFormData` encoding and also contains the encoding
+    ///            error.
+    public enum MultipartFormDataEncodingResult {
+        case success(request: UploadRequest, streamingFromDisk: Bool, streamFileURL: URL?)
+        case
