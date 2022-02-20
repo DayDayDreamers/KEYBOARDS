6584de8488cb511625295fff517232a02737fced
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2022-02-20 12:14:05 +0000

    Added model

--- Pods/Alamofire/Source/SessionManager.swift
+++ Pods/Alamofire/Source/SessionManager.swift
@@ -63,4 +63,10 @@ open class SessionManager {
             return "\(languageCode);q=\(quality)"
         }.joined(separator: ", ")
 
-        // User-Agent Header; see https://tools.ietf.org/html/rfc7231#section-5
+        // User-Agent Header; see https://tools.ietf.org/html/rfc7231#section-5.5.3
+        // Example: `iOS Example/1.0 (org.alamofire.iOS-Example; build:1; iOS 10.0.0) Alamofire/4.0.0`
+        let userAgent: String = {
+            if let info = Bundle.main.infoDictionary {
+                let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
+                let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
+                let appVer
