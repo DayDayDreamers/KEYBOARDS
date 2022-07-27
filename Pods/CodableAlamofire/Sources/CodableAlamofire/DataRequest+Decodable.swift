66d8b03ca742aa0ad7881f5059da70a9492929cd
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2022-07-27 20:28:18 +0000

    add missing package

--- Pods/CodableAlamofire/Sources/CodableAlamofire/DataRequest+Decodable.swift
+++ Pods/CodableAlamofire/Sources/CodableAlamofire/DataRequest+Decodable.swift
@@ -47,4 +47,15 @@ extension DataRequest {
         
         switch result {
         case .success(let json):
-            if let nestedJson = (json a
+            if let nestedJson = (json as AnyObject).value(forKeyPath: keyPath) {
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: nestedJson)
+                    let object = try decoder.decode(T.self, from: data)
+                    return .success(object)
+                }
+                catch {
+                    return .failure(error)
+                }
+            }
+            else {
+               
