1182bf42a07f6fb314329e65f235608e56931dcb
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2023-03-23 18:49:39 +0000

    Removing comments.

--- Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
+++ Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
@@ -5,4 +5,16 @@ set -o pipefail
 
 if [ -z ${UNLOCALIZED_RESOURCES_FOLDER_PATH+x} ]; then
     # If UNLOCALIZED_RESOURCES_FOLDER_PATH is not set, then there's nowhere for us to copy
-    # resou
+    # resources to, so exit 0 (signalling the script phase was successful).
+    exit 0
+fi
+
+mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
+> "$RESOURCES_TO_COPY"
+
+XCASSET_FILES=()
+
+# This protects against multiple targets copying the same framework dependency at the same time. The solution
+# was originally proposed here: https://lists.samba
