d416405acf1cb17eca05530ddc80b60f1409b93c
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2023-03-23 19:01:52 +0000

    - remove warnings

--- Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
+++ Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
@@ -17,4 +17,21 @@ RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
 XCASSET_FILES=()
 
 # This protects against multiple targets copying the same framework dependency at the same time. The solution
-# was originally proposed here: https://lists.samba
+# was originally proposed here: https://lists.samba.org/archive/rsync/2008-February/020158.html
+RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
+
+case "${TARGETED_DEVICE_FAMILY:-}" in
+  1,2)
+    TARGET_DEVICE_ARGS="--target-device ipad --target-device iphone"
+    ;;
+  1)
+    TARGET_DEVICE_ARGS="--target-device iphone"
+    ;;
+  2)
+    TARGET_DEVICE_ARGS="--target-device ipad"
+    ;;
+  3)
+    TARGET_DEVICE_ARGS="--target-device tv"
+    ;;
+  4)
+    TARGET_DEVICE_ARGS
