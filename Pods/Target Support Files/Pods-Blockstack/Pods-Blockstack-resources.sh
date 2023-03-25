3d193c626a2c4c844cc19457856ab46bbf2dc9a2
DayDayDreamers <93640231+DayDayDreamers@users.noreply.github.com>
2023-03-25 22:15:30 +0000

    Fix variable naming

--- Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
+++ Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-resources.sh
@@ -54,4 +54,7 @@ error: Resource "$RESOURCE_PATH" not found. Run 'pod install' to update the copy
 EOM
     exit 1
   fi
-  case $RESOURC
+  case $RESOURCE_PATH in
+    *.storyboard)
+      echo "ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .storyboard`.storyboardc $RESOURCE_PATH --sdk ${SDKROOT} ${TARGET_DEVICE_ARGS}" || true
+      ibtool --reference-external-
